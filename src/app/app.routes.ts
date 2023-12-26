eaa4357dabbdd5927c88033b6c01c3b205740879
Aziz <aziz.zina2001@gmail.com>
2023-12-26 23:10:21 +0100

    added not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  {
-    path: 'categories',
-    component: CategoriesComponent,
-  },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' },
 ];
