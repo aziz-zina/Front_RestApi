45ee3c98fbfa8a751a766430debc4c0abb4444f4
Aziz <aziz.zina2001@gmail.com>
2023-12-31 19:26:23 +0100

    added update functionnality

---
+++ src/app/update-product/update-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-product.component.html',
+  styleUrl: './update-product.component.css'
+})
+export class UpdateProductComponent {
+
+}
